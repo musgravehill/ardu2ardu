d82bc060625bb524520b757f6047c1c4e69b544b
musgravehill <musgravehill@gmail.com>
2015-11-05 11:18:02 +0400

    bytes

--- i2cMaster/i2cMaster.ino
+++ i2cMaster/i2cMaster.ino
@@ -1,20 +1,36 @@
 /*
 2 Arduinos crossed by A4-A4, A5-A5 (i2c)
-And common ground!
-What about 10k pullups on SDA, SCL? 
-*/
+ And common ground!
+ What about 10k pullups on SDA, SCL? 
+ */
 
 #include <Wire.h>
-int x = 0;
+uint8_t addressSlave0 = 0x1A; // 7bit address: max 127_DEC, 1111111_BIN
+
 void setup() {
   // Start the I2C Bus as Master
-  Wire.begin(); 
+  Wire.begin(); //address=null => Im Master on line
+  Serial.begin(9600);
+  
+  //sendBytesToSlave
+  byte dataBytes[] = { 0b11111111, 0b00000000, 0b11110000, 0b00001111 };
+  uint8_t dataSize = sizeof(dataBytes);
+  sendBytesToSlave(addressSlave0, dataBytes, dataSize);
 }
+
 void loop() {
-  Wire.beginTransmission(9); // transmit to device #9
-  Wire.write(x);              // sends x 
-  Wire.endTransmission();    // stop transmitting
-  x++; // Increment x
-  if (x > 3) x = 0; // `reset x once it gets 6
-  delay(500);
+  
 }
+
+//MAX 32 bytes due to buffer size in source ||& hardware limit
+void sendBytesToSlave(uint8_t addressSlave, const byte* dataBytes, size_t dataSize){
+  byte dataByte;
+  Wire.beginTransmission(addressSlave); // 7-bit slave address.
+  for(uint8_t i=0; i < dataSize; i++){
+    dataByte = dataBytes[i];
+    Wire.write(dataByte);  //send 1 byte to slave
+    Serial.println(dataByte, BIN);
+  }   
+  Wire.endTransmission(); //stop transmitting
+}
+
--- i2cSlave/i2cSlave.ino
+++ i2cSlave/i2cSlave.ino
@@ -1,10 +1,11 @@
 /*
 2 Arduinos crossed by A4-A4, A5-A5 (i2c)
-And common ground!
-What about 10k pullups on SDA, SCL? 
-*/
+ And common ground!
+ What about 10k pullups on SDA, SCL? 
+ */
 
 #include <Wire.h>
+addressSlave0 = 0x1A; // 7bit address: max 127_DEC, 1111111_BIN
 int LED1 = 3;
 int LED2 = 4;
 int x = 0;
@@ -13,7 +14,7 @@ void setup() {
   pinMode (LED1, OUTPUT);
   pinMode (LED2, OUTPUT);
   // Start the I2C Bus as Slave on address 9
-  Wire.begin(9); 
+  Wire.begin(9); // 7-bit slave address. Im slave.
   // Attach a function to trigger when something is received.
   Wire.onReceive(receiveEvent);
   Serial.begin(9600);
@@ -23,17 +24,23 @@ void receiveEvent(int bytes) {
   Serial.println(x, DEC);
 }
 void loop() {  
-  if (x == '0') {
-    digitalWrite(LED1, 1);    
-    digitalWrite(LED2, 0);    
+  if (x == 0) {
+    digitalWrite(LED1, HIGH);    
+    digitalWrite(LED2, LOW);   
+   delay(100); 
   }
-  if (x == '1') {
-    digitalWrite(LED1, 0);    
-    digitalWrite(LED2, 1);    
+  if (x == 1) {
+    digitalWrite(LED1, LOW);    
+    digitalWrite(LED2, HIGH);    
   }
-  if (x == '2') {
-    digitalWrite(LED1, 1);    
-    digitalWrite(LED2, 1);    
+  if (x == 2) {
+    digitalWrite(LED1, HIGH);    
+    digitalWrite(LED2, HIGH);    
   }  
-  
+  if (x == 3) {
+    digitalWrite(LED1, LOW);    
+    digitalWrite(LED2, LOW);    
+  }  
+
 }
+
