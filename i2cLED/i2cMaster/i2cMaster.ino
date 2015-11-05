18f346eb2700564157b5bbae2634fe19196a9017
musgravehill <musgravehill@gmail.com>
2015-11-05 15:23:55 +0400

    example LED

--- i2cLED/i2cMaster/i2cMaster.ino
+++ i2cLED/i2cMaster/i2cMaster.ino
@@ -1,104 +1,30 @@
 /*
 2 Arduinos crossed by A4-A4, A5-A5 (i2c)
  And common ground!
- What about 10k pullups on SDA, SCL? 
- 
- begin()
- begin(address)
- requestFrom(address, count)
- beginTransmission(address)
- endTransmission()
- send()
- available()
- receive()
- onReceive(handler)
- onRequest(handler)
- 
+ What about 10k pullups on SDA, SCL?  
  */
-
-#define DEBUG
 
 #include <Wire.h>
 uint8_t addressSlave0 = 0x1A; // 7bit address: max 127_DEC, 1111111_BIN
 
-void setup() {
-  // Start the I2C Bus as Master
+void setup() {  
   Wire.begin(); //address=null => Im Master on line
-
-#ifdef DEBUG 
-  Serial.begin(9600);
-#endif
-
 }
 
 void loop() {
-
   //sendBytesToSlave
-  byte dataBytes[] = { 
-    0b11111111, 0b00000000, 0b11110000, 0b00001111       };
+  byte slavePinsState = random(0,3); // 0b00 .. 0b11
+  byte dataBytes[] = { slavePinsState };
   uint8_t dataSize = sizeof(dataBytes);
-  //sendBytesToSlave(addressSlave0, dataBytes, dataSize);
-  delay(2000);
-
-  //Master wants to get bytes from a slave
-  uint8_t countBytesMustBe = 2;
-  byte* bytesFromSlave = requestBytesFromSlave(addressSlave0, countBytesMustBe);
-  byte dbg = *bytesFromSlave;
-  //Serial.println(dbg, BIN);
- // Serial.println(dbg[1], BIN);
-  delay(2000);
-
-
-}
-//Master wants to get bytes from a slave
-byte* requestBytesFromSlave(uint8_t addressSlave, uint8_t countBytesMustBe){  
-  uint8_t countBytesReceived;  
-  Wire.beginTransmission(addressSlave); // 7-bit slave address.
-  countBytesReceived = Wire.requestFrom(addressSlave, countBytesMustBe);
-  byte dataBytes[countBytesReceived];
-  
-  Serial.println("----------");
-  
-  //if we get required quantity of bytes
-  if(countBytesReceived == countBytesMustBe){
-    uint8_t i =0;
-    while(Wire.available()){
-      dataBytes[i] = Wire.read(); //read 1 byte
-      Serial.println(dataBytes[i], BIN);
-      i++;      
-    }
-  }
-  
-  Wire.endTransmission();
-
-#ifdef DEBUG 
-  Serial.print("countBytesReceived=");
-  Serial.print(countBytesReceived, DEC);
-  Serial.print("\r\n");
-#endif 
-  return dataBytes;
+  sendBytesToSlave(addressSlave0, dataBytes, dataSize);
+  delay(100);
 }
 
-//MAX 32 bytes due to buffer size in source ||& hardware limit
 void sendBytesToSlave(uint8_t addressSlave, const byte* dataBytes, size_t dataSize){
   byte dataByte;
   Wire.beginTransmission(addressSlave); // 7-bit slave address.
   Wire.write(dataBytes, dataSize);
   Wire.endTransmission(); //stop transmitting
-
-#ifdef DEBUG 
-  Serial.print("send bytesArray to slave ");
-  Serial.print(addressSlave, HEX);
-  Serial.print("\r\n");
-  Serial.print("countBytes=");
-  Serial.print(dataSize, DEC);
-  Serial.print("\r\n");
-  for(uint8_t i=0; i < dataSize; i++){
-    dataByte = dataBytes[i];    
-    Serial.println(dataByte, BIN);
-  }
-#endif
-
 }
 
 
--- i2cLED/i2cSlave/i2cSlave.ino
+++ i2cLED/i2cSlave/i2cSlave.ino
@@ -4,72 +4,36 @@
  What about 10k pullups on SDA, SCL? 
  */
 
-#define DEBUG 
-
 #include <Wire.h>
 uint8_t addressSlave0 = 0x1A; // 7bit address: max 127_DEC, 1111111_BIN
+const uint8_t ledPin1 = 3; 
+const uint8_t ledPin2 = 4;
 
-void setup() {
-  // Start the I2C Bus as Slave on address 9
+
+void setup() {  
   Wire.begin(addressSlave0); // 7-bit slave address. Im slave.  
   Wire.onReceive(wireOnReceive_event); //when Master send data to me  
-  Wire.onRequest(wireOnRequest_event);//when Master request data from me
-
-#ifdef DEBUG   
+  
   Serial.begin(9600);
-#endif  
+
+  pinMode(ledPin1, OUTPUT); 
+  pinMode(ledPin2, OUTPUT);   
 }
 
 void wireOnReceive_event(int countBytes) {
-#ifdef DEBUG 
-  Serial.print("countBytes=");
-  Serial.print(countBytes, DEC);
-  Serial.print("\r\n");
-#endif 
-
   byte currByte;  
   while(Wire.available()){
     currByte = Wire.read(); //read 1 byte
-
-#ifdef DEBUG 
-    Serial.println(currByte, DEC);
-#endif 
+    Serial.println(currByte, BIN);
   }
-
+  
+  digitalWrite(ledPin2, LOW);
+  
 }
+
 void loop() {    
 
 }
-
-void wireOnRequest_event(){
-  int input = analogRead(A1);
-
-  // To send multiple bytes from the slave,
-  // you have to fill your own buffer and send it all at once.
-  uint8_t buffer[2];
-  buffer[0] = input >> 8; //MSB, first 8 digits from 2bytes 0b****************
-  buffer[1] = input & 0xff;
-  
-  /*
-  //send
-  input = 10 001 000 11
-  input >> 8 = 10
-  input & 0xff = input & 11111111 = 001 000 11
-  //receive
-  b0 = 10;
-  b1 = 001 000 11;
-  
-  b0<<8 = 10 0000 0000;
-  
-  b0<<8 | b2 = 
-    10 0000 0000
-         10 0011  
-    -------------
-    10 0010 0011  === input
-   */    
-  Wire.write(buffer, 2);
-}
-
 
 
 
